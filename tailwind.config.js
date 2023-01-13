6e8dc192d01f92d6d33494220b240df7fd953f23
Trostynka <dimaploxyta@gmail.com>
2023-01-13 18:53:53 +0200

    continue using tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        'text': '1px 1px 0 rgba(0, 0, 0, 0.3)',
+      },
+
+    },
+    // backgroundSize:{
+    //   'customimg': 'url("C:\Users\Dmytro\Desktop\FlexMainProject\img\icons\icon.png") 0 1px no-repeat',
+    // },
   },
   plugins: [],
 }
